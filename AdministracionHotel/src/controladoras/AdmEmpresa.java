49a69c62e212b15a5e277df54bbd759ca92adea2
Carlos Vera <carlosvera2004@hotmail.com>
2012-06-09 00:14:32 -0500

    subo test de Status en clientes

--- AdministracionHotel/src/controladoras/AdmEmpresa.java
+++ AdministracionHotel/src/controladoras/AdmEmpresa.java
@@ -4,10 +4,40 @@
  */
 package controladoras;
 
+import java.util.ArrayList;
+import modelos.Agencia;
+import modelos.Empresa;
+
 /**
  *
  * @author CARLOS
  */
 public class AdmEmpresa {
     
+    private ArrayList<Empresa>empresas;
+
+     public AdmEmpresa() {
+        empresas=new ArrayList<Empresa>();
+    }
+
+    public ArrayList<Empresa> getEmpresas() {
+        return empresas;
+    }
+
+    
+     
+      public void registrarEmpresa(String codigo, String nombre,String telefono,String correo, String direccion, String ciudad, String estado, String pais){
+       buscarEmpresa(codigo);
+       Empresa empresa=new Empresa(codigo,nombre,telefono,correo,direccion,ciudad,estado,pais);
+       empresas.add(empresa);
+    }
+      
+      private void buscarEmpresa(String codigo)throws RuntimeException{
+        for(Empresa empresa:empresas){
+            if(empresa.getCodigo().equals(codigo))
+                throw new RuntimeException("CODIGO YA REGISTRADO");         
+        }
+    
+      }
+
 }
--- AdministracionHotel/test/controladoras/AdmClientesTest.java
+++ AdministracionHotel/test/controladoras/AdmClientesTest.java
@@ -3,8 +3,6 @@
  * and open the template in the editor.
  */
 package controladoras;
-
-import controladoras.AdmCliente;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -17,14 +15,14 @@ public class AdmClientesTest {
     
     @Test
     
-    public void SiNoExistenClientesStatusDebeSerCero(){
+    public void SiStatusEsCeroNoExistenClientesRegistrados(){
         AdmCliente admCliente=new AdmCliente();
         assertEquals(0, admCliente.getClientes().size());
     }
     
     @Test
     
-    public void SiExistenClientesStatusNoDebeSerCero(){
+    public void SiStatusNoEsCeroExistenClientesRegistrados(){
         AdmCliente admCliente=new AdmCliente();
         admCliente.registrarCliente("001","Carlos","Vera","4039972","5402532","masculino","carlosvera@hotmail.com","av. peru 1232","lima"," ","peru","08/04/1984","Cliente Frecuente");
         assertEquals(1, admCliente.getClientes().size());
@@ -32,16 +30,31 @@ public class AdmClientesTest {
     
     @Test
     
-    public void SiNoExistenAgenciasStatusDebeSerCero(){
+    public void SiStatusEsCeroNoExistenAgenciasRegistradas(){
         AdmAgencia admAgencia=new AdmAgencia();
         assertEquals(0, admAgencia.getAgencias().size());
     }
     
     @Test
     
-    public void SiExistenAgenciasStatusNoDebeSerCero(){
+    public void SiStatusNoEsCeroExistenAgenciasRegistradas(){
         AdmAgencia admAgencia=new AdmAgencia();
         admAgencia.registrarAgencia("100","Chazqui Wasi","2102532","chazquiwasi@gmail.com","av.primavera 1232","lima"," ","peru");
         assertEquals(1, admAgencia.getAgencias().size());
+    }
+    
+    @Test
+    
+    public void SiStatusEsCeroNoExistenEmpresasRegistradas(){
+        AdmEmpresa admEmpresa=new AdmEmpresa();
+        assertEquals(0, admEmpresa.getEmpresas().size());
+    }
+    
+    @Test
+    
+    public void SiStatusNoEsCeroExistenEmpresasRegistradas(){
+        AdmEmpresa admEmpresa=new AdmEmpresa();
+        admEmpresa.registrarEmpresa("200","Peru express","3879878","peruexpress@yahoo.com","av. lima 123","trujillo"," ","peru");
+        assertEquals(1, admEmpresa.getEmpresas().size());
     }
 }
